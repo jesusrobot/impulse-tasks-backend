42f1bfff5094c92a7d48061608506d0b40aad661
jesusrobot <jesusrobot9000@gmail.com>
2022-06-14 17:46:20 -0600

    Add the  User database model

---
+++ models/user.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const userSchema = Schema({
+  name: {
+    type: String,
+    required: [true, 'the "name" field is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'the "email" field is required'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'the "password" field is required'],
+  },
+  googleSignin: {
+    type: Boolean,
+    default: false,
+  },
+  role: {
+    type: String,
+    required: [true, 'the "role" field is required'],
+  },
+  profilePicture: {
+    type: String,
+  },
+  status: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+export default model("User", userSchema);
