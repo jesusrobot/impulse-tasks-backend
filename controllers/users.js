2c449d3038d51cf1d045c497aafaef9667a341fa
jesusrobot <jesusrobot9000@gmail.com>
2022-06-19 00:49:58 -0600

    Add endpoint to add user

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,17 @@
+import User from "../models/user.js";
+import bcryptjs from "bcryptjs";
+
 export const getUser = (req, res) => {
   res.json({ verb: "GET" });
 };
 
-export const createUser = (req, res) => {
-  res.json({ verb: "POST" });
+export const createUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  const user = new User({ name, email, password });
+  const salt = bcryptjs.genSaltSync();
+  user.password = bcryptjs.hashSync(password, salt);
+  await user.save();
+  res.json({ user });
 };
 
 export const updateUser = (req, res) => {
